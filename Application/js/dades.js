e973650db8302fce0ce9a2ba3e42c14d227d6221
Alex <xv500i@gmail.com>
2014-04-06 23:03:27 +0200

    not necessary anymore

--- Application/js/dades.js
+++
@@ -1,7 +0,0 @@
-var allBuffs = {};
-var allWeapons = {};
-var noStats = new Stadistics({});
-var automatic = new AlwaysCondition();
-allBuffs["Might 1"] = new Buff("Might 1", noStats, new Stadistics({"pAttack":5}), automatic);
-allWeapons["Tallum Blade"] = new Weapon("Tallum Blade", "A", new Stadistics({"pAttack": 125, "pCriticChance": 50}), noStats, "fast", "1h sword");
-allWeapons["Elysian"] = new Weapon("Elysian", "A", new Stadistics({"pAttack": 125, "pCriticChance": 25}), noStats, "fast", "1h blunt");
