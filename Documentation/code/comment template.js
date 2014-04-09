94acce7833c29ea04700aaa6284a9fdd185df763
Alex <xv500i@gmail.com>
2014-04-09 22:55:47 +0200

    Refactor directories and code documentation template

---
+++ Documentation/code/comment template.js
@@ -0,0 +1,34 @@
+/**
+ * Description about the file (Mandatory)
+ * @version x.x.x (Mandatory)
+ * @autor Alex Soms (Mandatory if it is not specified more concretely)
+ * @todo Something to do
+ * @copyright Alex Soms
+ * @see directory /Documentation
+ * @summary short description
+ */
+
+/**
+ * Function description (Mandatory)
+ * @version x.x.x (Mandatory)
+ * @author (Mandatory if it is not specified more generaly)
+ * @todo
+ * @see
+ * @constructor (Mandatory)
+ * @abstract
+ * @throws (Mandatory)
+ * @param {Number} param1 - Description of the param (Mandatory)
+ * @returns {String} - Description of the return (Mandatory)
+ * @since x.x.x
+ */
+function abc(param1) {
+	return "abc";
+}
+
+/**
+ * Variable description
+ * @member
+ * @todo
+ *
+ */
+var variable = "asb";
